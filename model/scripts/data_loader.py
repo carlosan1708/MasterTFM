1d9ffbe0923724cbf5a8cff6ef9a84707c396a14
Carlos Andres Rodriguez Trigueros <carlosan.1708@gmail.com>
2022-11-25 07:29:48 -0600

    Easier modifications

--- model/scripts/data_loader.py
+++ model/scripts/data_loader.py
@@ -35,15 +35,13 @@ def load_and_transform(TRAIN_IMAGE_DIR, TRAIN_MASK_DIR,TEST_IMAGE_DIR, TEST_MASK
             A.VerticalFlip(p=0.5),
             A.RandomRotate90(p=0.5),
             A.Transpose(p=0.5),
-            # A.ShiftScaleRotate(shift_limit=0.01, scale_limit=0.04, rotate_limit=0, p=0.25),
-            # A.RandomBrightnessContrast(p=0.5),
-            # A.RandomGamma(p=0.25),
-            # A.Blur(p=0.01, blur_limit = 2),
-            # A.OneOf([
-            #     A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
-            #     A.GridDistortion(p=0.5),
-            #     A.OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5)                  
-            # ],p=0.8),
+            A.RandomBrightnessContrast(p=0.5),
+            A.RandomGamma(p=0.25),
+            A.Blur(p=0.01, blur_limit = 2),
+            A.OneOf([
+                A.ElasticTransform(p=0.5, interpolation=cv2.INTER_NEAREST),
+                A.GridDistortion(p=0.5, distort_limit=[-0.3, 0.3])
+            ],p=0.9),
             ToTensorV2(),
         ],
     )
